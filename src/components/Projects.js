de3e9809a5dbdc42398b24e17f27684c75713091
kimtaeng9 <taenytaeni@gmail.com>
2024-10-16 22:11:35 +0900

    "초기 커밋 및 페이지, 컴포넌트 구조 설정"

---
+++ src/components/Projects.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Projects() {
+    return(
+        <div>
+            <h1>프로젝트</h1>
+            <p>목록</p>
+        </div>
+    )
+}
+
+export default Projects;
