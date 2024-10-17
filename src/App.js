5a737ce6e44060e9529c83f8bc17023c0c75ad6b
kimtaeng9 <taenytaeni@gmail.com>
2024-10-17 18:41:44 +0900

    "기본 라우팅과 네비게이션 바 설정"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<About />} />  
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+function Navigation() {
+    return (
+        <nav>
+            <ul>
+                <li><Link to = "/">About</Link></li>
+                <li><Link to ="/Skills">Skills</Link></li>
+                <li><Link to ="/projects">Projects</Link></li>
+                <li><Link to ="/contact">Contact</Link></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navigation;
