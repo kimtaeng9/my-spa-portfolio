41b35d8ee89a3eca930895b3aaca961f61b7da30
kimtaeng9 <taenytaeni@gmail.com>
2024-10-18 11:55:57 +0900

    "반응형 적용 및 이미지  추가, 리드미 수정" > npm 다시 설치

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,18 +5,19 @@ const projectData = [
     {
         id: 1,
         title: "Hotel Web", 
-        description: "Hotel Reservation Website built with Java, Spring Boot, JPA, H2, Mustache ",
+        description: "호텔 예약 사이트 Java, Spring Boot, JPA, H2, Mustache ",
         link: "https://github.com/kimtaeng9/HotelWeb",
-        image: "/path/to/image1.jpg"
+        image: process.env.PUBLIC_URL + "/images/img1.jpg"
     },
     {
         id: 2, 
         title: "My Portfolio Application", 
-        description: "My Portfolio Application built with React, JavaScript",
+        description: "내 포트폴리오 애플리케이션 React, JavaScript, AWS",
         link: "https://github.com/kimtaeng9/my-spa-portfolio",
-        image: "/path/to/image1.jpg"
+        image: process.env.PUBLIC_URL + "/images/img2.png"
     }
 ];
+
 
 function Projects() {
     return(
@@ -24,11 +25,13 @@ function Projects() {
             <h2>My Projects</h2>
             <div className="projects-container">
             {projectData.map((project) =>(
-            <div className="project-card" key={project.id}>
-                <img src={project.image} alt={project.title}></img>
-                <h3>{project.title}</h3>
-                <p>{project.description}</p>
-                <a href={project.link}>LEARN MORE</a>
+                <div className="project-card" key={project.id}>
+                <img src={project.image} alt={project.title}className="project-image" />
+                    <div className="project-details">
+                    <h3>{project.title}</h3>
+                    <p>{project.description}</p>
+                    <a href={project.link}>LEARN MORE</a>
+                    </div>
             </div>
             ))}
             </div>
